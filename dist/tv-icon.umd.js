891a1e5bd0ff99d7758de688844c342305a4587d
kevin olson <acidjazz@gmail.com>
2021-06-12 16:53:41 -0500

    🎨 dark mode for mixins

--- dist/tv-icon.umd.js
+++ dist/tv-icon.umd.js
@@ -384,17 +384,17 @@ var staticRenderFns = []
     primary: {
       type: String,
       required: false,
-      default: 'text-gray-400',
+      default: 'text-gray-400 dark:text-gray-600',
     },
     secondary: {
       type: String,
       required: false,
-      default: 'text-gray-300',
+      default: 'text-gray-300 dark:text-gray-500',
     },
     third: {
       type: String,
       required: false,
-      default: 'text-indigo-100',
+      default: 'text-indigo-100 dark:text-indigo-600',
     },
   },
 });
--- src/mixins/icon.js
+++ src/mixins/icon.js
@@ -3,17 +3,17 @@ export default {
     primary: {
       type: String,
       required: false,
-      default: 'text-gray-400',
+      default: 'text-gray-400 dark:text-gray-600',
     },
     secondary: {
       type: String,
       required: false,
-      default: 'text-gray-300',
+      default: 'text-gray-300 dark:text-gray-500',
     },
     third: {
       type: String,
       required: false,
-      default: 'text-indigo-100',
+      default: 'text-indigo-100 dark:text-indigo-600',
     },
   },
 }
